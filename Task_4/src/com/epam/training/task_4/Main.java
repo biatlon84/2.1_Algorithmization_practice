f49b9d42a2e4ebb3f23b725b9553994de43ff21a
kwas <kwas@kwas-MS-7641>
2021-07-13 16:52:53 +0300

    2.1_Algorithmization_practice

---
+++ Task_4/src/com/epam/training/task_4/Main.java
@@ -0,0 +1,34 @@
+package com.epam.training.task_4;
+
+//You are given real numbers a1, a2, ..., an.
+//Swap the largest and smallest items.
+public class Main {
+	public static void main(String[] args) {
+		double array[] = new double[] { -0.001, 4.23, 1.876, 0.34, -456.3, 0, -234.1 };
+		int min = 0, max = 0;
+		double bu = -111111111110f, bumin = 10000000000f;
+
+		for (int i = 0; array.length > i; i++) {
+			if (array[i] > bu) {
+				bu = array[i];
+				max = i;
+			}
+			if (bumin > array[i]) {
+				bumin = array[i];
+				min = i;
+			}
+		}
+		array[min] = bu;
+		array[max] = bumin;
+		System.out.println("max " + bu);
+		System.out.println("min " + bumin);
+		int i = 0;
+		while (i < array.length) {
+			System.out.print(" " + array[i]);
+			i++;
+			if (i % 10 == 0)
+				System.out.print("\n");
+		}
+	}
+
+}
